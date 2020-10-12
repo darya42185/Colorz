5091a137081e217630ff9bf5d519103e165dabfb
Solomaha Darya <solomaha_darya@ukr.net>
2020-10-12 15:58:35 +0300

    first commit v1

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-btn').onclick = function() {
+  this.classList.toggle('menu-btn_active');
+}
+
+document.querySelector('.rotate').onclick = function() {
+  this.classList.toggle('rotate_active');
+}
